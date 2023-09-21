5cbc06823fc6d9e35ce1ef64f5a89b2b7bb97ab0
hammed amusat oriyomi <mederhoo@gmail.com>
2023-09-21 03:03:02 +0100

    simple shell

---
+++ test.c
@@ -0,0 +1,20 @@
+/*
+ * File: simple_shell.c
+ * Auth: ChatGPT
+ * Desc: A simple shell program.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point for the simple shell program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("hello")
+	return (0);
+}
